d8691603e712223917c4f62439a110f3d1bef191
jarturomora <jarturomora@gmail.com>
2015-02-20 20:04:35 -0600

    code-snipets improved to create a unique data set

--- code-snipets.R
+++ code-snipets.R
@@ -1,7 +1,23 @@
+#######################################################################
+#Task 1: Merges the training and the test sets to create one data set.#
+#######################################################################
 # Step 1. Load data into dataframes for manipulation
-# Loading column names
+# Loading feature names
 featureNames <- read.table("data/features.txt")
+# Load "training" data
+x_trainingData <- read.table("data/train/X_train.txt")
+y_trainingData <- read.table("data/train/y_train.txt")
+voluteer_trainData <- read.table("data/train/subject_train.txt")
+
 # Loading "tests" data
-testData <- read.table("data/test/X_test.txt")
-# Adding column names to test data
-colnames(testData) <- featureNames[,2]
+x_testsData <- read.table("data/test/X_test.txt")
+y_testsData <- read.table("data/test/y_test.txt")
+voluteer_testData <- read.table("data/test/subject_test.txt")
+
+# Create a unique data set with traing and tests data
+x_data <- rbind(x_trainingData, x_testsData)
+y_data <- rbind(y_trainingData, y_testsData)
+volunteer_data <- rbind(voluteer_trainData, voluteer_testData)
+
+# Adding feature names to data set
+colnames(x_data) <- featureNames[,2]
